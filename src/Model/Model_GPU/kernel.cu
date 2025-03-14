d6eaa03afb36c454d5ea2932acf48d2f52367e93
Pepiovesan100 <pedropiovesan2000@gmail.com>
2025-03-14 11:28:38 +0100

    wip shared mem

--- src/Model/Model_GPU/kernel.cu
+++ src/Model/Model_GPU/kernel.cu
@@ -12,27 +12,49 @@ __global__ void compute_acc(float3 * positionsGPU, float3 * velocitiesGPU, float
 	accelerationsGPU[i].x = 0.0f;
 	accelerationsGPU[i].y = 0.0f;
 	accelerationsGPU[i].z = 0.0f;
+	__shared__  float3 posj[128];
+	__shared__  float masses[128];
+	float3 posi = positionsGPU[i];
+	float3 acc = {0.0f, 0.0f, 0.0f};
 
-	for(int j = 0; j < n_particles; j++){
-		if(i != j){
-			const float diffx = positionsGPU[j].x - positionsGPU[i].x;
-			const float diffy = positionsGPU[j].y - positionsGPU[i].y;
-			const float diffz = positionsGPU[j].z - positionsGPU[i].z;
-
-			float dij = diffx * diffx + diffy * diffy + diffz * diffz;
-
-			if (dij < 1.0){
-				dij = 10.0f;
-			} else {
-				dij = std::sqrt(dij);
-				dij = 10.0 / (dij * dij * dij);
-			}
-
-			accelerationsGPU[i].x += diffx * dij * massesGPU[j];
-			accelerationsGPU[i].y += diffy * dij * massesGPU[j];
-			accelerationsGPU[i].z += diffz * dij * massesGPU[j];
+	for(int block = 0; block < (n_particles + blockDim.x - 1) / blockDim.x; block++){
+		int j = block*blockDim.x + threadIdx.x;
+		if (j < n_particles) {
+			posj[threadIdx.x] = positionsGPU[j];
+			masses[threadIdx.x] = massesGPU[j];
+		} else {
+			posj[threadIdx.x] = {0.0f, 0.0f, 0.0f};
+			masses[threadIdx.x] = 0.0f;
 		}
+
+		__syncthreads();
+
+		for(int k = 0; k < blockDim.x; k++){
+			if (!(block == blockIdx.x && k == threadIdx.x)) {
+				
+				const float diffx = posj[k].x - posi.x;
+				const float diffy = posj[k].y - posi.y;
+				const float diffz = posj[k].z - posi.z;
+
+				float dij = diffx * diffx + diffy * diffy + diffz * diffz;
+
+				if (dij < 1.0){
+					dij = 10.0f;
+				} else {
+					dij = std::sqrt(dij);
+					dij = 10.0 / (dij * dij * dij);
+				}
+
+				acc.x += diffx * dij * masses[k];
+				acc.y += diffy * dij * masses[k];
+				acc.z += diffz * dij * masses[k];
+			}
+		}
+
+		__syncthreads();
 	}
+
+	accelerationsGPU[i] = acc;
 }
 
 __global__ void maj_pos(float3 * positionsGPU, float3 * velocitiesGPU, float3 * accelerationsGPU, int n_particles)
@@ -55,7 +77,7 @@ void update_position_cu(float3* positionsGPU, float3* velocitiesGPU, float3* acc
 	int nblocks =  (n_particles + (nthreads -1)) / nthreads;
 
 	compute_acc<<<nblocks, nthreads>>>(positionsGPU, velocitiesGPU, accelerationsGPU, massesGPU, n_particles);
-	// cudaDeviceSynchronize();
+	cudaDeviceSynchronize();
 	maj_pos    <<<nblocks, nthreads>>>(positionsGPU, velocitiesGPU, accelerationsGPU, n_particles);
 }
 
