4f5a3807f71357477ea98a490f62574dda6bfcaf
Pepiovesan100 <pedropiovesan2000@gmail.com>
2025-03-21 17:47:44 +0100

    unroll

--- src/Model/Model_GPU/kernel.cu
+++ src/Model/Model_GPU/kernel.cu
@@ -3,12 +3,10 @@
 #include "cuda.h"
 #include "kernel.cuh"
 
-#define DIFF_T (0.1f)
-#define THD (1024)
+#define THD (256)
 
-__global__ void compute_acc(float4 * positionsGPU, float3 * accelerationsGPU, float3 * velocitiesGPU, int n_particles){
+__global__ void compute_acc(float4 * positionsGPU, float3 * accelerationsGPU, int n_particles){
 	unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
-	// if (i >= n_particles)return;
 	float4 posi;
 	if (i >= n_particles){
 		posi = (float4){0.0f, 0.0f, 0.0f, 0.0f};
@@ -18,17 +16,21 @@ __global__ void compute_acc(float4 * positionsGPU, float3 * accelerationsGPU, fl
 	
 	float3 acc = {0.0f, 0.0f, 0.0f};
 
-
 	__shared__ float4 shared_particles[THD];
 
+	#pragma unroll 128
 	for (int j = 0; j < n_particles; j += blockDim.x) {
 		// Load a tile of particles into shared memory
 		int k = j + threadIdx.x;
-		shared_particles[threadIdx.x] = positionsGPU[k];
+		if (k < n_particles) {
+			shared_particles[threadIdx.x] = positionsGPU[k];
+		} else {
+			shared_particles[threadIdx.x] = make_float4(0.0f, 0.0f, 0.0f, 0.0f);
+		}
 		
 		__syncthreads(); // Ensure all threads have loaded the tile
 
-		
+		#pragma unroll 64
 		for (int l = 0; l < blockDim.x; l++) {
 			int idx = j + l; // Global index of the particle
 			if (idx >= n_particles) break;
@@ -39,8 +41,9 @@ __global__ void compute_acc(float4 * positionsGPU, float3 * accelerationsGPU, fl
 			const float diffz = posj.z - posi.z;
 
 			float dij = diffx * diffx + diffy * diffy + diffz * diffz;
-			// float dij = fmaf(diffx, diffx, fmaf(diffy, diffy, diffz*diffz));
 			
+			// float inv_dij = __frsqrt_rn(fmaxf(dij, 1.0f));
+            // float mul = __fmul_rn(posj.w, __fmul_rn(inv_dij, __fmul_rn(inv_dij, inv_dij))) * 10.0f;
 			dij = rsqrtf(fmaxf(dij,1.0f));
 			dij = 10.0f * (dij * dij * dij);
 
@@ -49,10 +52,6 @@ __global__ void compute_acc(float4 * positionsGPU, float3 * accelerationsGPU, fl
 			acc.x = fmaf(diffx, mul, acc.x);
 			acc.y = fmaf(diffy, mul, acc.y);
 			acc.z = fmaf(diffz, mul, acc.z);
-
-			// acc.x += diffx * dij * posj.w
-			// acc.y += diffy * dij * posj.w;
-			// acc.z += diffz * dij * posj.w;
 		}
 
 		__syncthreads(); // Ensure all threads load the data before computation
@@ -80,7 +79,7 @@ void update_position_cu(float4* positionsGPU, float3* velocitiesGPU, float3* acc
 	int nthreads = THD;
 	int nblocks =  (n_particles + (nthreads -1)) / nthreads;
 	
-	compute_acc<<<nblocks, nthreads>>>(positionsGPU, accelerationsGPU, velocitiesGPU, n_particles);
+	compute_acc<<<nblocks, nthreads>>>(positionsGPU, accelerationsGPU, n_particles);
 	cudaDeviceSynchronize();
 	maj_pos    <<<nblocks, nthreads>>>(positionsGPU, velocitiesGPU, accelerationsGPU, n_particles);
 }
